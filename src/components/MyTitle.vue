0aed5714184cf348332ce183c69ef224b13ec4fa
Yomakota <teoperitore@outlook.com>
2022-07-05 18:01:54 +0200

    add MyTitle

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>La mia prima app con vue CLI</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyTitle',
+}
+</script>
+
+<style lang="scss" scoped>
+    h2 {
+        color: red;
+    }
+</style>
