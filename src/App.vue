c1f71fb45b41bc9a13bf6219efd22049b6863b41
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-06 12:55:41 +0200

    comment HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <MyTitle />
     <NewHero />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import MyTitle from './components/MyTitle.vue'
 import NewHero from './components/NewHero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     MyTitle,
     NewHero,
   }
